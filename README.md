25f44bb91cce1563dc357dc27e721f1dd4f45b11
Roy Six <roysix@users.noreply.github.com>
2018-11-03 17:59:30 -0400

    .

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # xframe-same-origin
+An extension that sets X-Frame-Options headers to "SAME ORIGIN", overriding any potential "DENY" values set by the server.
+This is useful for other extensions that need this behavior like Infy Scroll, and it should be relatively safe as compared to allowing cross-origins.
---
+++ src/chrome/js/background.js
@@ -0,0 +1,43 @@
+/**
+ * X-Frame Same Origin
+ * @file background.js
+ * @author Roy Six
+ * @license LGPL-3.0
+ */
+
+var Background = (() => {
+
+  /**
+   * The chrome.webRequest.onHeadersReceived listener that is added if scroll TODO is enabled.
+   * Fired when HTTP response headers of a request have been received.
+   * Changes the HTTP Header X-Frame-Options value to SAME-ORIGIN to allow the use of embedded iframes on the page.
+   * This is needed if a website sends a X-Frame-Options with a value of DENY or ALLOW-FROM.
+   * X-Frame-Options: DENY, SAMEORIGIN, ALLOW-FROM https://example.com
+   *
+   * @param details object containing details of the headers received
+   * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+   * @private
+   */
+  function webRequestOnHeadersReceivedListener(details) {
+    console.log("webRequestOnHeadersReceivedListener() - the chrome.webRequest.onHeadersReceived listener is on!");
+    return {
+      responseHeaders: details.responseHeaders.map(header => {
+        console.log("webRequestOnHeadersReceivedListener() - header:" + header.name + "=" + header.value);
+        const headerName = header.name.toLowerCase(),
+              headerValue = header.value.toLowerCase();
+        if (headerName === "x-frame-options") {
+          header.value = "SAMEORIGIN";
+          console.log("webRequestOnHeadersReceivedListener() - changed:" + header.name + "=" + header.value);
+        } else if (headerName === "content-security-policy" && headerValue.includes("frame-ancestors")) {
+          // todo check frame-src and frame-ancestors and frame... who knows...
+          header.value = "self";
+        }
+        return header;
+      })
+    };
+  }
+
+  // Background Listeners
+  chrome.webRequest.onHeadersReceived.addListener(webRequestOnHeadersReceivedListener, { urls: ["<all_urls>"], types: ["main_frame", "sub_frame"]}, ["blocking", "responseHeaders"]);
+
+})();
